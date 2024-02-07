a61c8f1a6c8ff1e1cf67c414a55b35bde043a4f0
Jennifer Takagi <jennifer.takagi@personio.de>
2024-02-07 14:03:39 +0100

    [chore] Setup Router Layouts to app

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,29 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { AppLayout } from './pages/_layouts/AppLayout'
+import { AuthLayout } from './pages/_layouts/AuthLayout'
 import { DashboardPage } from './pages/app/DashboardPage'
 import { SignInPage } from './pages/auth/SignInPage'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <DashboardPage />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <DashboardPage />,
+      },
+    ],
   },
   {
-    path: '/sign-in',
-    element: <SignInPage />,
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/sign-in',
+        element: <SignInPage />,
+      },
+    ],
   },
 ])
