bfb81ce34e85c0b7c1234d86c9b741c8886df3e5
Jennifer Takagi <jennifer.takagi@personio.de>
2024-02-07 13:27:15 +0100

    [chore] Set up shadcn/ui as component library

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
 export const App = () => {
   return (
-    <h1>Pizza Shop</h1>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Order pizza</Button>
+    </div>
   )
 }
