ff23231cb431e90b1eec1914bc505d1cc2a6be8d
Jennifer Takagi <jennifer.takagi@personio.de>
2024-02-17 15:47:18 +0100

    [feat] Apply optimistic update on restaurant edition

--- frontend/src/server-state/hooks/usePutRestaurant/usePutProfile.tsx
+++ frontend/src/server-state/hooks/usePutRestaurant/usePutProfile.tsx
@@ -2,26 +2,45 @@ import { GetRestaurantResponse } from "@/server-state/api/get-restaurant"
 import { updateRestaurant } from "@/server-state/api/update-restaurant"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 
-export const usePutProfile =() => {
+function updateRestaurantCache({
+  name,
+  description,
+}: {
+  name: string,
+  description: string | null
+}) {
   const queryClient = useQueryClient()
+  const cached = queryClient.getQueryData<GetRestaurantResponse>([
+    'pizza-shop-app-restaurant',
+  ])
+
+  if (cached) {
+    queryClient.setQueryData<GetRestaurantResponse>(
+      ['pizza-shop-app-restaurant'],
+      {
+        ...cached,
+        name,
+        description,
+      },
+    )
+  }
+
+  return { cached }
+}
+
+export const usePutProfile =() => {
 
   return useMutation({
     mutationFn: updateRestaurant,
-    onSuccess(_, { description, name }) {
-      const cached = queryClient.getQueryData<GetRestaurantResponse>([
-        'pizza-shop-app-restaurant',
-      ])
+    onMutate({ description, name }) {
+      const { cached } = updateRestaurantCache({ description, name })
 
-      if (cached) {
-        queryClient.setQueryData<GetRestaurantResponse>(
-          ['pizza-shop-app-restaurant'],
-          {
-            ...cached,
-            name,
-            description,
-          },
-        )
-      }
+      return { previousProfile: cached }
     },
+    onError(_, __, context) {
+      if (context?.previousProfile) {
+        updateRestaurantCache(context.previousProfile)
+      }
+    }
   })
 }
