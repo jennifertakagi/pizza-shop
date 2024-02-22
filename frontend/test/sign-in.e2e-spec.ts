0fb750a228dc664ea14791149bc8f64acdf42171
Jennifer Takagi <jennifer.takagi@personio.de>
2024-02-22 11:12:54 +0100

    [test] Add e2e test to SignIn page

---
+++ frontend/test/sign-in.e2e-spec.ts
@@ -0,0 +1,33 @@
+import { expect, test } from '@playwright/test'
+
+test('sign in successfully', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Email').fill('johndoe@example.com')
+  await page.getByRole('button', { name: 'Access panel' }).click()
+
+  const toast = page.getByText(
+    'We have sent an authentication link to your email',
+  )
+
+  expect(toast).toBeVisible()
+})
+
+test('sign in with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Email').fill('wrong@example.com')
+  await page.getByRole('button', { name: 'Access panel' }).click()
+
+  const toast = page.getByText('Invalid credential')
+
+  expect(toast).toBeVisible()
+})
+
+test('navigate to new restaurant page', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'New restaurant' }).click()
+
+  expect(page.url()).toContain('/sign-up')
+})
