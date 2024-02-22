f6dd5bc2a0df0e435fc17c342133e503fb6a8eed
Jennifer Takagi <jennifer.takagi@personio.de>
2024-02-22 09:15:59 +0100

    [test] Add test to SignInPage component

---
+++ frontend/src/pages/auth/SignInPage/SignInPage.spec.tsx
@@ -0,0 +1,32 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { render } from '@testing-library/react'
+import { HelmetProvider } from 'react-helmet-async'
+import { MemoryRouter } from 'react-router-dom'
+
+import { queryClient } from '@/lib/react-query'
+
+import { SignInPage } from './SignInPage'
+
+describe('SignInPage', () => {
+  it('should set default email input value if email is present on search params', () => {
+    const wrapper = render(<SignInPage />, {
+      wrapper: ({ children }) => {
+        return (
+          <HelmetProvider>
+            <MemoryRouter
+              initialEntries={['/sign-in?email=johndoe@example.com']}
+            >
+              <QueryClientProvider client={queryClient}>
+                {children}
+              </QueryClientProvider>
+            </MemoryRouter>
+          </HelmetProvider>
+        )
+      },
+    })
+
+    const emailInput = wrapper.getByLabelText('Email') as HTMLInputElement
+
+    expect(emailInput.value).toEqual('johndoe@example.com')
+  })
+})
